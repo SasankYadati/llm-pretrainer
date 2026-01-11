3c50d811eb3d5bdb23c74a0bf7dc9fbc1150f443
Sai Sasank Y <sai.sasank.yadati@gmail.com>
2026-01-11 14:04:24 +0530

    comment out overriding

--- train.py
+++ train.py
@@ -37,24 +37,24 @@ if __name__ == '__main__':
 
     # model config
     parser.add_argument("--model_name", type=str, default="HuggingFaceTB/SmolLM-360M-Instruct")
-    parser.add_argument("--num_hidden_layers", type=int, default=32)
-    parser.add_argument("--num_attention_heads", type=int, default=16)
-    parser.add_argument("--num_key_value_heads", type=int, default=4)
+    # parser.add_argument("--num_hidden_layers", type=int, default=32)
+    # parser.add_argument("--num_attention_heads", type=int, default=16)
+    # parser.add_argument("--num_key_value_heads", type=int, default=4)
 
     # training
     parser.add_argument("--seed", type=int, default=42)
     parser.add_argument("--learning_rate", type=float, default=3e-4)
     parser.add_argument("--seq_len", type=int, default=1024)
-    parser.add_argument("--micro_batch_size", type=int, default=32)
+    parser.add_argument("--micro_batch_size", type=int, default=16)
     parser.add_argument("--gradient_accumulation_steps", type=int, default=4)
     parser.add_argument("--dataset_name", type=str, default="roneneldan/TinyStories")
-    parser.add_argument("--n_tokens", type=int, default=int(1e6))
+    parser.add_argument("--n_tokens", type=int, default=int(2e6))
     parser.add_argument("--num_workers", type=int, default=1)
     parser.add_argument("--num_proc", type=int, default=4)
 
 
     # logging
-    parser.add_argument("--run_name", type=str, default="default_run")
+    parser.add_argument("--run_name", type=str, default="no_parallelism")
 
     args = parser.parse_args()
 
@@ -64,7 +64,7 @@ if __name__ == '__main__':
 
     wandb.init(
         project="llm-pretrainer",
-        name=f"{args.run_name}_no_parallelism",
+        name=f"{args.run_name}",
         config={
             "tensor_parallel_size": 1, # no parallelism
             "pipeline_parallel_size": 1,
